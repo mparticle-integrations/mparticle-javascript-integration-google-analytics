5a5bdf6d6b19f818b314ce1b2bff28be57a63935
Robert Ing <robertming@gmail.com>
2019-11-21 09:42:41 -0500

    Update version number

--- dist/GoogleAnalyticsEventForwarder.common.js
+++ dist/GoogleAnalyticsEventForwarder.common.js
@@ -18,7 +18,7 @@ Object.defineProperty(exports, '__esModule', { value: true });
 
     var name = 'GoogleAnalyticsEventForwarder',
         moduleId = 6,
-        version = '2.0.1',
+        version = '2.0.2',
         MessageType = {
             SessionStart: 1,
             SessionEnd: 2,
--- dist/GoogleAnalyticsEventForwarder.iife.js
+++ dist/GoogleAnalyticsEventForwarder.iife.js
@@ -17,7 +17,7 @@ var mpGoogleAnalyticsKit = (function (exports) {
 
         var name = 'GoogleAnalyticsEventForwarder',
             moduleId = 6,
-            version = '2.0.1',
+            version = '2.0.2',
             MessageType = {
                 SessionStart: 1,
                 SessionEnd: 2,
