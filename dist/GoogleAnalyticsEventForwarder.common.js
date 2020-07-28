24dfa1d291b1baaebc804ed9541eeb626efb0d69
mParticle Automation <developers@mparticle.com>
2020-07-28 22:23:26 +0000

    Generate latest bundle

--- dist/GoogleAnalyticsEventForwarder.common.js
+++ dist/GoogleAnalyticsEventForwarder.common.js
@@ -18,7 +18,7 @@ Object.defineProperty(exports, '__esModule', { value: true });
 
     var name = 'GoogleAnalyticsEventForwarder',
         moduleId = 6,
-        version = '2.0.6',
+        version = '2.0.7',
         MessageType = {
             SessionStart: 1,
             SessionEnd: 2,
--- dist/GoogleAnalyticsEventForwarder.iife.js
+++ dist/GoogleAnalyticsEventForwarder.iife.js
@@ -17,7 +17,7 @@ var mpGoogleAnalyticsKit = (function (exports) {
 
         var name = 'GoogleAnalyticsEventForwarder',
             moduleId = 6,
-            version = '2.0.6',
+            version = '2.0.7',
             MessageType = {
                 SessionStart: 1,
                 SessionEnd: 2,
